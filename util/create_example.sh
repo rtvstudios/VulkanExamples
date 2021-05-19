17e77b47787450e3e388468e738b7db317c71fa5
Ranjith <ranjith_t@hcl.com>
2021-05-19 11:05:06 -0400

    example creation util added

---
+++ util/create_example.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function ASSERT_SUCCESS {
+	if [ $? -ne 0 ]
+	then
+		echo $1
+		exit 1
+	fi
+}
+
+if [ "$1" = "" ]
+then
+	echo "Usage: $0 <project_name>"
+	exit 1
+fi
+
+PROJECT_NAME=$1
+CREATION_DATE=`date`
+
+mkdir $PROJECT_NAME
+ASSERT_SUCCESS "Could not create project directory $PROJECT_NAME"
+
+mkdir $PROJECT_NAME
+FILE_CONTENT=`cat CMakeLists_Template.txt`
+
+touch $PROJECT_NAME/CMakeLists.txt
+
+function CreateFile {
+	while read line; do    
+		echo "$line" | sed -e "s/\$PROJECT_NAME/$PROJECT_NAME/" | sed -e "s/\$CREATION_DATE/$CREATION_DATE/" >> $PROJECT_NAME/$2
+	done < $1
+
+}
+
+CreateFile CMakeLists_Template.txt CMakeLists.txt
+CreateFile main_template.cpp main.cpp
+
---
+++ util/main_template.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  $PROJECT_NAME
+//
+//  Created by Ranjith on $CREATION_DATE
+//
+
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
+
+int main(int, const char **) {
+	return 0;
+}
+
